e640b1ea810082504886e8ff68e2c034fc99545f
Silvio Ruiz <silviojrck@gmail.com>
2023-06-08 14:17:31 -0300

    Agrega validaciones al formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,19 +1,36 @@
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 import CardTempertatura from './CardTempertatura';
+import { useForm } from 'react-hook-form';
 
 const Formulario = () => {
 
-    
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
+    }
 
     return (
         <Container className='mt-5 text-center'>
-            <Form className='text-light '>
+            <Form className='text-light' onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group as={Row}>
                     <Form.Label column sm="10" md="4">
                          Elegir una ciudad
                     </Form.Label>
                     <Col sm="10" md="6">
-                        <Form.Control placeholder='ciudad'></Form.Control>
+                        <Form.Control placeholder='ciudad' type='text' {...register("city", {
+                            required: true,
+                            maxLength: 30,
+                            minLength: 3,
+                            pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/u
+                        })}></Form.Control>
+                        <div className='error-container'>
+                            {errors.city?.type === "required" && <div className='error-msg'>Debe ingresar una ciudad</div>}
+                            {errors.city?.type === "maxLength" && <div className='error-msg'>No debe superar los 30 caracteres</div>}
+                            {errors.city?.type === "minLength" && <div className="error-msg">Debe contener 3 caracteres o mas</div>}
+                            {errors.city?.type === "pattern" && <div className='error-msg'>Debe ingresar un nombre válido</div>}
+                        </div>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row} className='my-4'>
@@ -21,7 +38,18 @@ const Formulario = () => {
                          Elegir un país
                     </Form.Label>
                     <Col sm="10" md="6">
-                        <Form.Control placeholder='pais'></Form.Control>
+                        <Form.Control placeholder='pais' type='text' {...register("country", {
+                            required: true,
+                            maxLength: 40,
+                            minLength: 3,
+                            pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/u
+                        })}></Form.Control>
+                        <div className='error-container'>
+                            {errors.country?.type === "required" && <div className='error-msg'>Debe ingresar un país</div>}
+                            {errors.country?.type === "maxLength" && <div className='error-msg'>No debe superar los 40 caracteres</div>}
+                            {errors.country?.type === "minLength" && <div className="error-msg">Debe contener 3 caracteres o mas</div>}
+                            {errors.country?.type === "pattern" && <div className='error-msg'>Debe ingresar un nombre válido</div>}
+                        </div>
                     </Col>
                 </Form.Group>
                 <Button type='submit'>Consultar clima</Button>
