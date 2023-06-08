52f3ce206dfc88567aa51edc04b3d75144f9c049
Silvio Ruiz <silviojrck@gmail.com>
2023-06-08 15:58:26 -0300

    Agrega validacion y temperatura en celcius

--- src/components/CardTempertatura.jsx
+++ src/components/CardTempertatura.jsx
@@ -7,17 +7,24 @@ import rainImg from "../assets/clear.jpg";
 
 const CardTempertatura = ({ weatherInfo }) => {
 
+
+    if (weatherInfo.cod !== "200") {
+        return <h2 className='text-light'>No se encontró esa ciudad</h2>
+    }
+
     const weatherImages = {
         Clear: clearImg,
         Clouds: cloudImg,
         Rain: rainImg,
     };
 
+    const tempInCelcius = (weatherInfo.main.temp - 273).toFixed(2);
+
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={weatherImages[weatherInfo.weather[0].main]} />
             <Card.Body className='text-center'>
-                <Card.Title>{ weatherInfo.main.temp - 273}°</Card.Title>
+                <Card.Title>{ tempInCelcius }°</Card.Title>
                 <Card.Text>{ weatherInfo.weather[0].main}</Card.Text>
                 <Card.Text className='p-3'>
                     { weatherInfo.name }
