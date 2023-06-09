8046a2a0bc3e54c4bdd1b5be3c80f25ca7df628c
Silvio Ruiz <silviojrck@gmail.com>
2023-06-09 09:34:02 -0300

    Modifica la url de la peticion

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,7 +1,7 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 
 export const getWeatherInfo = async(city, country) => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ city },${ country }&appid=${ apiKey }`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ city },${ country }&appid=${ apiKey }`;
     try{
         const response = await fetch(url);
         const data = await response.json();
